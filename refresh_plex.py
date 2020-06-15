1d1d8cd964aee0d7614745fdf4bed8b23cd85211
John Ross <jlross42@gmail.com>
2020-06-14 18:31:30 -0700

    switch to yaml config file

--- refresh_plex.py
+++ refresh_plex.py
@@ -4,33 +4,41 @@ import os
 import sys
 from pathlib import PurePath
 from pathlib import PurePosixPath
+from typing import List
+from typing import NamedTuple
 from typing import Union
 
-
-LIBRARIES = ["movies", "tv"]
+import yaml
 
 PathLike = Union[PurePath, os.PathLike]
+PlexLibrary = NamedTuple("PlexLibrary", [("src", PathLike), ("dest", PathLike)])
 
 
 class Config:
     def __init__(self, parsed_args):
-        self.src_base_dir: PurePath = PurePath(parsed_args.src_base_dir)
-        self.dest_base_dir: PurePath = PurePath(parsed_args.dest_base_dir)
-        self.plex_host_string: str = parsed_args.plex_host
+        self.config_file: PathLike = PurePath(parsed_args.config)
+        self.plex_libs: List[PlexLibrary] = []
+        self.plex_host: str = parsed_args.plex_host
         self.plex_bin_dir: PurePosixPath = PurePosixPath(parsed_args.plex_bin_dir)
         self.dry_run: bool = parsed_args.dry_run
         self.skip_plex_scan: bool = parsed_args.skip_plex_scan
         self.verbose: bool = parsed_args.verbose
 
-    def validate(self) -> bool:
-        is_valid = True
-        for lib in LIBRARIES:
-            for base_dir in [self.src_base_dir, self.dest_base_dir]:
-                lib_dir: PathLike = base_dir.joinpath(lib)
-                if not os.path.exists(lib_dir):
-                    logging.error(f"lib dir is missing: {lib_dir}")
-                    is_valid = False
-        return is_valid
+    def parse_config_file(self):
+        with open(self.config_file) as fp:
+            config_dict = yaml.safe_load(fp)
+        for lib_dict in config_dict["libs"]:
+            lib = PlexLibrary(PurePath(lib_dict["src"]), PurePath(lib_dict["dest"]))
+            is_valid = True
+            if not os.path.exists(lib.src):
+                is_valid = False
+                logging.error(f"lib src is missing: {lib.src}")
+            if not os.path.exists(lib.dest):
+                is_valid = False
+                logging.error(f"lib dest is missing: {lib.dest}")
+            if is_valid:
+                self.plex_libs.append(lib)
+        self.plex_host = config_dict.get("plex_host", "localhost")
 
 
 class Plex:
@@ -41,13 +49,13 @@ class Plex:
         self,
         root: str,
         name: str,
-        src_lib_dir: PathLike,
-        dest_lib_dir: PathLike,
+        lib_src: PathLike,
+        lib_dest: PathLike,
         is_dirs: bool,
     ) -> bool:
         dest_path: PathLike = PurePath(root).joinpath(name)
-        rel_path: PathLike = dest_path.relative_to(dest_lib_dir)
-        src_path: PathLike = src_lib_dir.joinpath(rel_path)
+        rel_path: PathLike = dest_path.relative_to(lib_dest)
+        src_path: PathLike = lib_src.joinpath(rel_path)
         logging.debug(f"checking if {dest_path} is removed")
         if not os.path.exists(src_path):
             if is_dirs:
@@ -62,16 +70,11 @@ class Plex:
         return False
 
     def check_added_media(
-        self,
-        root: str,
-        name: str,
-        src_lib_dir: PathLike,
-        dest_lib_dir: PathLike,
-        is_dirs,
+        self, root: str, name: str, lib_src: PathLike, lib_dest: PathLike, is_dirs,
     ) -> bool:
         src_path: PathLike = PurePath(root).joinpath(name)
-        rel_path: PathLike = src_path.relative_to(src_lib_dir)
-        dest_path: PathLike = dest_lib_dir.joinpath(rel_path)
+        rel_path: PathLike = src_path.relative_to(lib_src)
+        dest_path: PathLike = lib_dest.joinpath(rel_path)
         logging.debug(f"checking if {src_path} is added")
         if not os.path.exists(dest_path):
             if is_dirs:
@@ -86,53 +89,31 @@ class Plex:
         return False
 
     def sync(self):
-        metrics = {}
-        for lib in LIBRARIES:
-            metrics[lib] = {
-                "removed": {"dirs": 0, "files": 0},
-                "added": {"dirs": 0, "files": 0},
-            }
-            src_lib_dir: PathLike = self.config.src_base_dir.joinpath(lib)
-            dest_lib_dir: PathLike = self.config.dest_base_dir.joinpath(lib)
-            if not os.path.exists(src_lib_dir):
-                logging.error(f"src lib dir is missing: {src_lib_dir}")
-            if not os.path.exists(dest_lib_dir):
-                logging.error(f"dest lib dir is missing: {dest_lib_dir}")
-
-            lib_metrics = metrics[lib]["removed"]
-            for root, dirs, files in os.walk(dest_lib_dir):
+        metrics = {"removed": {"dirs": 0, "files": 0}, "added": {"dirs": 0, "files": 0}}
+        for lib in self.config.plex_libs:
+            lib_metrics = metrics["removed"]
+            for root, dirs, files in os.walk(lib.dest):
                 for dir in dirs:
-                    if self.check_removed_media(
-                        root, dir, src_lib_dir, dest_lib_dir, True
-                    ):
+                    if self.check_removed_media(root, dir, lib.src, lib.dest, True):
                         dirs.remove(dir)
                         lib_metrics["dirs"] += 1
                 for file in files:
-                    if self.check_removed_media(
-                        root, file, src_lib_dir, dest_lib_dir, False
-                    ):
+                    if self.check_removed_media(root, file, lib.src, lib.dest, False):
                         lib_metrics["files"] += 1
 
-            lib_metrics = metrics[lib]["added"]
-            for root, dirs, files in os.walk(src_lib_dir):
+            lib_metrics = metrics["added"]
+            for root, dirs, files in os.walk(lib.src):
                 for dir in dirs:
-                    if self.check_added_media(
-                        root, dir, src_lib_dir, dest_lib_dir, True
-                    ):
+                    if self.check_added_media(root, dir, lib.src, lib.dest, True):
                         lib_metrics["dirs"] += 1
                 for file in files:
-                    if self.check_added_media(
-                        root, file, src_lib_dir, dest_lib_dir, False
-                    ):
+                    if self.check_added_media(root, file, lib.src, lib.dest, False):
                         lib_metrics["files"] += 1
 
-        for lib in LIBRARIES:
-            for section in ["removed", "added"]:
-                dirs_metric = metrics[lib][section]["dirs"]
-                files_metric = metrics[lib][section]["files"]
-                logging.info(
-                    f"{lib} {section} dirs={dirs_metric}, files={files_metric}"
-                )
+        for section in ["removed", "added"]:
+            dirs_metric = metrics[section]["dirs"]
+            files_metric = metrics[section]["files"]
+            logging.info(f"{section} dirs={dirs_metric}, files={files_metric}")
 
     def scan_and_refresh(self):
         pass
@@ -140,15 +121,7 @@ class Plex:
 
 def parse_args(args_without_script) -> Config:
     parser = argparse.ArgumentParser(description="synchronizes plex media folders")
-    parser.add_argument(
-        "--src-base-dir", "-S", required=True, help="the location of the actual media",
-    )
-    parser.add_argument(
-        "--dest-base-dir", "-D", required=True, help="the location of the links"
-    )
-    parser.add_argument(
-        "--plex-host", "-H", default="localhost", help="location of plexmediaserver"
-    )
+    parser.add_argument("--config", "-c", required=True, help="path to config file")
     parser.add_argument(
         "--dry-run",
         "-T",
@@ -174,12 +147,9 @@ if __name__ == "__main__":
         logging.getLogger("invoke").setLevel(logging.ERROR)
     if config.dry_run:
         logging.info("Doing a dry run, nothing is modified")
-    is_valid = config.validate()
-    if is_valid:
-        config.prompt()
+    config.parse_config_file()
     plex = Plex(config)
-    plex.sync()
-    if not is_valid:
-        sys.exit(1)
-    if not config.skip_plex_scan:
+    if not config.plex_libs:
+        logging.warning("No libraries to sync")
+        plex.sync()
         plex.scan_and_refresh()
